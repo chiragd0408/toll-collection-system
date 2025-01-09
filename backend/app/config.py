b3cb55ab128a0b43a0d7abc7f7dc2c6ef3ebedc2
chiragd0408 <chiragranpise48@gmail.com>
2025-01-09 10:48:29 +0530

    ..

--- backend/app/config.py
+++ backend/app/config.py
@@ -24,3 +24,4 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SECRET_KEY = 'your_development_secret_key'
 
+# The configuration can be selectezd based on environment variables or other conditions
